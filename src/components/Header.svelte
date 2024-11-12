32c0a4dc4c18004d0aaf514a8ae487d7185ea509
Lev Lavryniuk <leoyt109@gmail.com>
2024-11-12 09:42:48 +0200

    updates

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -10,14 +10,15 @@
 </script>
 
 <header
-	class="absolute left-0 top-0 z-10 h-40 w-screen bg-slate-900 p-4 text-center text-2xl font-bold text-white"
+	class="absolute left-0 top-0 z-10 flex h-40 w-screen flex-col items-center gap-5 bg-slate-900 p-4 text-center text-2xl font-bold text-white"
 >
+	<img src="/logo.webp" alt="logo" class="h-auto w-60" />
 	<MenuToggle bind:checked={isBurgerOpen} />
 	{#if isBurgerOpen}
 		<div
 			transition:slide
 			style:opacity={isBurgerOpen ? 1 : 0}
-			class="absolute left-0 top-20 h-fit w-full rounded-b-full bg-slate-900/80 pb-20 backdrop-blur-md duration-500"
+			class="absolute left-0 top-40 h-fit w-full rounded-b-full bg-slate-900/80 pb-20 pt-10 backdrop-blur-md duration-500"
 		>
 			<ul class="header-list">
 				<li><a {onclick} href="/">ГОЛОВНА</a></li>
